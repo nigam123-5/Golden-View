d5c2a44d438b60c525b48bf48b06902d90b50998
asifsiddiqui9011 <asifsiddiqui9011@gmail.com>
2025-02-21 12:29:54 +0530

    updated rooms and guestd etails

---
+++ public/js/roomsData.js
@@ -0,0 +1,52 @@
+const rooms = [
+    {
+        id: 1,
+        image: '/room1.jpg',
+        title: 'LUXURY ROOM',
+        description: 'These cozy rooms located in the historic Palace Wing are the gateway to a memorable experience...',
+        size: '33 sq mt',
+        wifi: 'Inclusive WiFi',
+        guests: 'Up to 3 guests',
+        bed: 'King',
+        highlight: 'Spacious rooms and bathrooms.',
+        price: '₹ 14,000'
+    },
+    {
+        id: 2,
+        image: '/room2.jpg',
+        title: 'EXECUTIVE SUITE',
+        description: 'These thematic suites offer an exclusive host of amenities such as access to buffet breakfast...',
+        size: '70 sq mt',
+        wifi: 'Inclusive WiFi',
+        guests: 'Up to 3 guests',
+        bed: 'King',
+        highlight: 'Classic decor providing a sense of history',
+        price: '₹ 10,000'
+    },
+    {
+        id: 3,
+        image: '/room4.jpg',
+        title: 'DELUXE SUITE',
+        description: 'They exude an aura of the old-world charm and elegance...',
+        size: '87 sq mt',
+        wifi: 'Inclusive WiFi',
+        guests: 'Up to 3 guests',
+        bed: 'King',
+        highlight: 'Grand architecture, spacious rooms and bathrooms.',
+        price: '₹ 15,000'
+    },
+    {
+        id: 4,
+        image: '/room3.jpg',
+        title: 'SIGNATURE SUITE',
+        description: 'These storied suites each have a unique history and offer breathtaking views...',
+        size: '121 sq mt',
+        wifi: 'Inclusive WiFi',
+        guests: 'Up to 4 guests',
+        bed: 'King',
+        highlight: 'Unique suite offering a Rajasthani splendour',
+        price: '₹ 12,000'
+    }
+];
+
+module.exports = rooms; // Export rooms data
