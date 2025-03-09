da48acbf099155b7c9d3efd9bed47ed7bdeb7b0c
Suryansh Nigam <nigamsuryansh11@gmail..com>
2025-03-09 20:43:37 +0530

    updated

--- public/js/script_hotel.js
+++ public/js/script_hotel.js
@@ -11,3 +11,50 @@ function myfunction(){
         click_content.style.overflow = 'visible';
     }
 }
+
+
+document.addEventListener("DOMContentLoaded", function () {
+    const stars = document.querySelectorAll(".star");
+
+    stars.forEach(star => {
+        star.addEventListener("mouseover", function () {
+            let value = this.getAttribute("data-value");
+            highlightStars(value);
+        });
+
+        star.addEventListener("mouseout", function () {
+            resetStars();
+        });
+
+        star.addEventListener("click", function () {
+            let value = this.getAttribute("data-value");
+            setRating(value);
+        });
+    });
+
+    function highlightStars(value) {
+        stars.forEach(star => {
+            if (star.getAttribute("data-value") <= value) {
+                star.classList.add("checked");
+            } else {
+                star.classList.remove("checked");
+            }
+        });
+    }
+
+    function resetStars() {
+        stars.forEach(star => {
+            star.classList.remove("checked");
+        });
+    }
+
+    function setRating(value) {
+        stars.forEach(star => {
+            if (star.getAttribute("data-value") <= value) {
+                star.classList.add("checked");
+            } else {
+                star.classList.remove("checked");
+            }
+        });
+    }
+});
